eeaca1b053b39c4bd99d2c7b710ba61d223f6370
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-09-12 17:46:08 -0700

    Estructura del proyecto
    -- Se agregaron los paquetes del negocio:
       -- dtos
       -- entidades
       -- negocio
       -- persistencia
       -- presentacion
       -- utilerias

---
+++ src/main/java/com/mycompany/proyecto1_ia/Proyecto1_IA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_ia;
+
+/**
+ *
+ * @author PC
+ */
+public class Proyecto1_IA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
