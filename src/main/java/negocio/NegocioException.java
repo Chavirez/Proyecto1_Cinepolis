804059a5cc2e692c1314d1fce7d4bfff41e76a25
vvcrxyz <nomar.limon04@gmail.com>
2024-09-29 00:09:19 -0700

    Documentaacion

--- src/main/java/negocio/NegocioException.java
+++ src/main/java/negocio/NegocioException.java
@@ -2,6 +2,9 @@ package negocio;
 
 public class NegocioException extends Exception {
 
+    /** Constructor que crea una nueva instancia de NegocioException con un mensaje específico
+
+@param mensaje El mensaje que describe la excepcion */
     public NegocioException(String mensaje) {
         super(mensaje);
     }
--- src/main/java/negocio/PeliculaNegocio.java
+++ src/main/java/negocio/PeliculaNegocio.java
@@ -20,10 +20,19 @@ public class PeliculaNegocio implements IPeliculaNegocio{
     
     private IPeliculasDAO peliculaDAO;
 
+    /** Constructor que inicializa una nueva instancia de PeliculaNegocio con el DAO de peliculas
+
+@param peliculaDAO El objeto que maneja la persistencia de las peliculas */
     public PeliculaNegocio(IPeliculasDAO peliculaDAO) {
         this.peliculaDAO = peliculaDAO;
     }
     
+    
+    /** Busca peliculas en la base de datos por sucursal y las devuelve como una lista de objetos PeliculaDTO
+
+@param idSucursal El ID de la sucursal para filtrar las peliculas 
+* @return Una lista de objetos PeliculaDTO que representa las peliculas obtenidas de la base de datos 
+* @throws NegocioException Si ocurre un error durante la busqueda en la base de datos */
     @Override
     public List<PeliculaDTO> buscarPeliculaTabla(int idSucursal) throws NegocioException {
          try {
@@ -36,6 +45,13 @@ public class PeliculaNegocio implements IPeliculaNegocio{
         }
     }
     
+    
+    /** Convierte una lista de objetos PeliculaEntidad en una lista de objetos PeliculaDTO para ser utilizados 
+     * en la presentacion de datos, como en una tabla
+
+@param peliculas La lista de objetos PeliculaEntidad que representan las peliculas obtenidas de la base de datos 
+* @return Una lista de objetos PeliculaDTO que contiene la informacion de las peliculas convertidas para su visualizacion 
+* @throws NegocioException Si la lista de peliculas es null, lanzara una excepcion indicando que no se pudieron obtener las peliculas */
     public List<PeliculaDTO> convertirPeliculasTablaDTO(List<PeliculaEntidad> peliculas) throws NegocioException {
         if (peliculas == null) {
             throw new NegocioException("No se pudieron obtener las sucursales");
@@ -56,6 +72,11 @@ public class PeliculaNegocio implements IPeliculaNegocio{
         return peliculaDTO;
     }    
     
+    
+    /** Busca todas las peliculas en la base de datos y las devuelve como una lista de objetos PeliculaDTO
+
+@return Una lista de objetos PeliculaDTO que representa todas las peliculas obtenidas de la base de datos 
+* @throws NegocioException Si ocurre un error durante la busqueda en la base de datos */
     @Override
     public List<PeliculaDTO> buscarPeliculaTablaT() throws NegocioException {
          try {
